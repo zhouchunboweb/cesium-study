cbd9a5f48a62364e17ec195b0857885cb7792ca2
zhouchunboweb <153566945@qq.com>
2020-05-12 11:39:58 +0800

    create study

---
+++ src/components/homePage/homePage.html.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="page-title">主页</div>
+    <div id="project">
+    </div>
+  </div>
+</template>
+<style>
+.page-title {
+  font-size: 20px;
+}
+</style>
---
+++ src/components/homePage/homePage.vue
@@ -0,0 +1,22 @@
+<script>
+import homePageHtml from './homePage.html'
+let Cesium = require('../../../node_modules/cesium/Build/Cesium/Cesium')
+import 'cesium/Widgets/widgets.css'
+export default {
+  name: 'homePage',
+  extends: homePageHtml,
+  data () {
+    return {
+      viewer: null
+    }
+  },
+  methods: {
+    initMap () {
+      this.viewer = new Cesium.Viewer('project')
+    }
+  },
+  mounted () {
+    this.initMap()
+  }
+}
+</script>
